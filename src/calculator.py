abe82aa3efb1a942ffb11caa44fccded3000bb77
Karthik B <karthikbgpillai@ibm.com>
2026-04-30 17:23:52 +0530

    changed sub function

--- src/calculator.py
+++ src/calculator.py
@@ -8,7 +8,7 @@ def add(a: int, b: int) -> int:
 
 def subtract(a: int, b: int) -> int:
     """Return the difference of two inteeeeegers and floats."""
-    return a - b
+    return a+b
 
 
 def divide(a: int, b: int) -> float:
