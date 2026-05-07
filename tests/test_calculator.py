11cc00f04abecc6361478893f2a1887d59476a71
Karthik B <karthikbgpillai@gmail.com>
2026-05-07 08:03:11 +0530

    pos-final-test (#18)
    * pos-final-test
    
    * checking sub folder discovery
    
    ---------
    
    Co-authored-by: Karthik B <karthikbgpillai@ibm.com>

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from src.calculator import add, divide, subtract
+from src.calculator import add, divide, subtract, power
 
 
 def test_add() -> None:
@@ -16,6 +16,8 @@ def test_subtract() -> None:
 def test_divide() -> None:
     assert divide(12, 3) == 4
 
+def test_power() -> None:
+    assert power(2, 3) == 8
 
 def test_divide_by_zero() -> None:
     with pytest.raises(ValueError, match="Cannot divide by zero"):
